7426227f180487025b6c5eda6d37375b7ffa11ef
Pavel M <marininpa@mail.ru>
2026-03-16 00:05:44 +0400

    add hw8_e3.c

--- hw8/hw8_e3.c
+++ hw8/hw8_e3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int in_arr(int *arr);
-int f_min_arr(int *arr, int &arr_min_pos);
-int f_max_arr(int *arr, int &arr_max_pos);
+int f_min_arr(int *arr, int *arr_min_pos);
+int f_max_arr(int *arr, int *arr_max_pos);
 
 enum {arr_size = 9};
 
@@ -15,8 +15,8 @@ int main(void)
 
   int arr[arr_size] = {};
   in_arr(arr);
-  min_arr = f_min_arr(arr, &arr_min_pos);
-  max_arr = f_max_arr(arr, &arr_max_pos);
+  arr_min = f_min_arr(arr, &arr_min_pos);
+  arr_max = f_max_arr(arr, &arr_max_pos);
   printf("%d %d %d %d\n", arr_max_pos, arr_max, arr_min_pos, arr_min);
   return 0;
 }
@@ -30,12 +30,36 @@ int in_arr(int *arr)
 }
 
 
-int f_min_arr(int *arr, int &arr_min_pos)
+int f_min_arr(int *arr, int *arr_min_pos)
 {
-  int i;
-  int min = arr[0];
+  int i = 0;
+  int min = arr[i];
+  *arr_min_pos = 1;
   for(i=0;i<=arr_size-1;i++)
+  {
     if (min > arr[i+1])
+    {
       min = arr[i+1];
+      *arr_min_pos = i+2;
+    }
+  }    
+     
   return min;
+}
+
+int f_max_arr(int *arr, int *arr_max_pos)
+{
+  int i = 0;
+  int max = arr[i];
+  *arr_max_pos = 1;
+  for(i=0;i<=arr_size-1;i++)
+  {
+    if (max < arr[i+1])
+    {
+      max = arr[i+1];
+      *arr_max_pos = i+2;
+    }
+  }   
+      
+  return max;
 }
