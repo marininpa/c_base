a71bbc4b6198686e08f5de4f2a6e03d0b62d7582
Pavel_M <marininpa@mail.ru>
2026-03-16 08:11:09 +0400

    edit e4

--- hw8/hw8_e4.c
+++ hw8/hw8_e4.c
@@ -9,9 +9,10 @@ int main(void)
 {
   int sum_max_arr = 0; 
   int arr[arr_size] = {};
+
   in_arr(arr);
   f_sort_arr(arr);
-  sum_max_arr = arr[arr_size] + arr[arr_size - 1];
+  sum_max_arr = arr[arr_size - 1] + arr[arr_size - 2];
   printf("%d\n", sum_max_arr);
   return 0;
 }
@@ -29,20 +30,20 @@ int f_sort_arr (int *arr)
   int i =0;
   int k =0;
   int tmp = 0;
-  int no_change = 0;
+  int no_change = 1;
   for (k=0;k<arr_size-1;k++)
   {
-    for (i=0;i<arr_size-1;i++)
+    for (i=0;i<arr_size-1-k;i++)
       {
         if (arr[i] > arr[i+1])
         {
           tmp = arr[i];
           arr[i] = arr[i+1];
           arr[i+1] = tmp;
-          no_change = 1;
+          no_change = 0;
         }
       }
-    return 0 ? no_change == 0 : no_change == 1;
-    no_change = 0;
+    if (no_change) break;
+    no_change = 1;
    }
 }
