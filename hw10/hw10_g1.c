b935cef5e7123876cf43455392b22c433e46745d
Pavel_M <marininpa@mail.ru>
2026-04-07 20:44:42 +0400

    edit hw10_g1.c

--- hw10/hw10_g1.c
+++ hw10/hw10_g1.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
-
+enum {size = 1000};
 
 int main(void)
 {
-FILE *f_in("input.txt", )
+char file_string[size] = {};
+int count_char = 0, tmp_char =0;
+FILE *f_in, *f_out;  
+if ((f_in = fopen("input.txt", "r")) == NULL) 
+  {
+    printf("Ошибка открытия файла input.txt\n");
+    return -1;
+  }
+if ((f_out = fopen("output.txt", "r")) == NULL)
+  {
+    printf("Ошибка открытия файла output.txt\n");
+    fclose(f_in);
+    return -1;
+  }
+//read input file
+while((tmp_char=fgetc(f_in))!=EOF)
+ {
+  file_string[count_char] = tmp_char;
+  count_char++;
+ }
 
 
-  return 0;
+// close file
+if (f_in != NULL) fclose(f_in);
+if (f_out != NULL) fclose(f_out);
+return 0;
 }
