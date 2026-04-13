fccab7f2d770217f039a8ce5ea899aeb791f4a7f
Pavel_M <marininpa@mail.ru>
2026-04-13 06:31:15 +0400

    edit hw10_g4.c

--- hw10/hw10_g4.c
+++ hw10/hw10_g4.c
@@ -5,8 +5,7 @@ enum {size = 101};
 int main(void)
 {
 
-int len = 0;
-int count =0;
+char check_s[128] = {0};
 char file_string1[size] = {};
 char file_string2[size] = {};
 
@@ -33,14 +32,27 @@ if((fscanf(f_in, "%s %s", file_string1, file_string2))!=2)
   return 1;
  }
 
- // общая длина строки с учётом \n
-len = strlen(file_string);
-last_s = file_string[len-2];
-for (count=0;count<len-2;count++)
-{
- if (file_string[count] == last_s) fprintf(f_out,"%d ", count);
+ // помечаем все символы в первом слове
+ for (int i=0;file_string1[i];i++)
+ {
+  check_s[file_string1[i]] = 1;
+ }
 
+// сравниваем с символами второго слова
+int s=0;
+for (int i=0;file_string2[i];i++)
+{
+  s = check_s[file_string2[i]] + 1;
+  check_s[file_string2[i]] = s;
 }
+
+// вывод повторяющихся символов
+for (int i = 0;i<127;i++)
+{
+  if (check_s[i] == 2) 
+  printf("%c ", i);
+}
+printf("\n");
 
 // close file
 if (f_in != NULL) fclose(f_in);
