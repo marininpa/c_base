43e8dd413f259a319ae7414816a6f3d6ab7becbc
Pavel_M <marininpa@mail.ru>
2026-04-18 13:17:48 +0400

    final version hw10_g4

--- hw10/hw10_g4.c
+++ hw10/hw10_g4.c
@@ -5,12 +5,13 @@ enum {size = 101};
 int main(void)
 {
 
-char check_s[128] = {0};
+char check_s1[128] = {0};
+char check_s2[128] = {0};
 char file_string1[size] = {};
 char file_string2[size] = {};
 
 
-FILE *f_in, *f_out;  
+FILE *f_in, *f_out;
 if ((f_in = fopen("input.txt", "r")) == NULL) 
   {
     printf("Ошибка открытия файла input.txt\n");
@@ -32,35 +33,42 @@ if((fscanf(f_in, "%s %s", file_string1, file_string2))!=2)
   return 1;
  }
 
- // помечаем все символы в первом слове
+ // помечаем все символы из первого слова с подсчётом кол-ва повторений
  int s=0;
  for (int i=0;file_string1[i];i++)
  {
-  s = check_s[file_string1[i]] + 1;
+  s = check_s1[file_string1[i]] + 1;
   if (s<2)
   {
-    check_s[file_string1[i]] = s;
+    check_s1[file_string1[i]] = s;
   }
   else 
   {
-    check_s[file_string1[i]] = 100;
+    check_s1[file_string1[i]] = 100;
   }
  }
 
-// сравниваем с символами второго слова
+// помечаем все символы из первого слова с подсчётом кол-ва повторений
 s=0;
 for (int i=0;file_string2[i];i++)
-{
-  s = check_s[file_string2[i]] + 1;
-  check_s[file_string2[i]] = s;
-}
+ {
+  s = check_s2[file_string2[i]] + 1;
+  if (s<2)
+  {
+    check_s2[file_string2[i]] = s;
+  }
+  else 
+  {
+    check_s2[file_string2[i]] = 100;
+  }
+ }
 
-// вывод повторяющихся символов
-for (int i = 0;i<127;i++)
-{
-  if (check_s[i] == 2)
-  fprintf(f_out,"%c ", i);
-}
+ // сравнение первого и второго слова, вывод повторяющихся
+ for (int i=0;i<127;i++)
+ {
+  if (check_s1[i] + check_s2[i] == 2) fprintf(f_out,"%c ", i); 
+ }
+
 
 // close file
 if (f_in != NULL) fclose(f_in);
